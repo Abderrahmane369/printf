8a8133176fa9847c9f1ad42a034eaaec2789b64c
Abderrahmane369 <cszamasu@gmail.com>
2023-07-21 16:06:28 -0700

    PRINTF HAS BEEN CREATED

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _printf - a printf-like function
+ * @format: a character string.
+ * @...: format string
+ * Return: number of characters in format
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
