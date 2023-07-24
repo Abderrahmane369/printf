a9a11f2dc7e539ce83d8e3e93ba1e69881eb2333
Abderrahmane369 <cszamasu@gmail.com>
2023-07-23 19:02:38 -0700

    VERSION 2

--- main.h
+++ main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
-#define  MAIN_H
+#define MAIN_H
 
 int _printf(const char *format, ...);
 void percent(void);
 void print_char(char c);
 void print_string(char *s);
+void print_int(int d);
+char *intToString(int n);
 
 #endif
